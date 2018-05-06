c834e6a35b1835860db6205963bcabfc90204f1e
Tony Ho <ho.tonym@gmail.com>
2018-05-06 19:08:22 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
-if 60 < speed && speed < 40
-	return true
-else
-	return false
-end
+	if 60 < speed || speed < 40
+		true
+	else
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
-	60 < speed && speed < 40 ?  true :  false
+	60 < speed || speed < 40 ? true : false
 end
